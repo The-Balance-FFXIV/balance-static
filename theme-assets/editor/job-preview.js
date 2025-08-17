8e53585b45c1f601db6e5aec527e378618bcbbf3
kromuluss <kromuluss@users.noreply.github.com>
2025-08-17 01:45:41 +0000

    deploy: 5cacc15d10e9304563243057eaa4af8d9654b4e3

--- theme-assets/editor/job-preview.js
+++ theme-assets/editor/job-preview.js
@@ -15,6 +15,36 @@ const renderGuideContainer = function (body, ...children) {
   );
 };
 
+const renderBisList = function (bis) {
+  const bisList = bis;
+
+  return h(
+    "div", {}, bisList.map(function (bis, indexer) {
+        const name = h("h2", {}, bis.name);
+        const type = bis.type;
+        const link = bis.link;
+        const description = h("p", {}, bis.description);
+        const bisFrame =
+          type != "plaintext"
+            ? h("div", { class: "h-96" }, h("iframe", {
+                src: 
+                  type === "etro" ? `https://etro.gg/embed/gearset/${link}` : 
+                  link,
+                class: "w-full h-full",
+              }))
+              : link;
+
+        return h(
+          "div",
+          { key: indexer, id: `bis-preview-${indexer}`, },
+          name,
+          bisFrame,
+          description,
+        );
+      })
+    )
+};
+
 const renderAuthorList = function (authors) {
   let authorList = authors ?? [];
   return h(
@@ -39,6 +69,17 @@ let GenericJobGuide = createClass({
       this.props.widgetFor("body"),
       h("hr", {}),
       renderAuthorList(authors)
+    );
+  },
+});
+
+let bisSetTemplate = createClass({
+  render: function () {
+    const bis = this.props.entry.getIn(["data", "bis"]);
+    const bisList = typeof bis.toJS === "function" ? bis.toJS() : bis;
+
+    return renderGuideContainer(
+      renderBisList(bisList)
     );
   },
 });
